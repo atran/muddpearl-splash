1faa89c0cfbf643ddf874cdf3145b9bb4e763bbd
anthony tran <atran.dummy@gmail.com>
2021-05-27 12:00:13 -0400

    html edits

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,18 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Muddpearl</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        {/* <Header title="Welcome to my app!" /> */}
+        <p>Coming soon</p>
+        <img src="/muddpearl_soon.jpg" />
+        <h1><a href="http://instagram.com/muddpearl/">@muddpearl</a></h1>
       </main>
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
