f236dc2a0b3b5804eec82e789b4b9b40ab48e8f4
anthony tran <atran.dummy@gmail.com>
2021-05-27 12:20:51 -0400

    css edits

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,28 @@ export default function Home() {
       <Head>
         <title>Muddpearl</title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-6KWHW6S8HM`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-6KWHW6S8HM', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
       </Head>
 
       <main>
         {/* <Header title="Welcome to my app!" /> */}
-        <p>Coming soon</p>
+        <p>coming soon</p>
         <a href="http://instagram.com/muddpearl/">
           <img src="/muddpearl_soon.jpg" />
           <h1>@muddpearl</h1>
